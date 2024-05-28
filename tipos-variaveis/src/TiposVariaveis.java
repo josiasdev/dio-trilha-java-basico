0169763998dc2dd229e26c9b32c265fabc1b9845
josiasdev <josiasmartins098@gmail.com>
2024-05-28 13:55:02 -0300

    atualização no arquivo TiposVariaveis.java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -16,7 +16,7 @@ public class TiposVariaveis {
 
         // <Tipo> <nomeVariavel> <atribuicaoDeValorOpcional>
         byte idade = 123;
-		short ano = 2021;
+		short ano = 2024;
 		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
 		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
 		float pi = 3.14F;
