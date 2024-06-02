6099ca5184560f6af083c83c0edca22836267517
josiasdev <josiasmartins098@gmail.com>
2024-06-02 13:26:14 -0300

    exemplo de ternário

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,44 +1,68 @@
 public class Operadores {
     public static void main(String[] args) {
-        String nome = "Josias";
-        int idade = 20;
-        double peso = 90;
-        char sexo = 'M';
-        boolean doadorOrgao = false;
-        
-        double soma = 10.5 + 15.7;
-        int subtração = 113 - 25;
-        int multiplicação = 20 * 7;
-        int divisao = 15 / 3;
-        int modulo = 18 % 3;
-        double resultado = (10 * 7 ) + (20 / 4);
-        String concatenacao = "?";
-        concatenacao = 1+1+1+"1";
+        // String nome = "Josias";
+        // int idade = 20;
+        // double peso = 90;
+        // char sexo = 'M';
+        // boolean doadorOrgao = false;
 
-        System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+1;
+        // double soma = 10.5 + 15.7;
+        // int subtração = 113 - 25;
+        // int multiplicação = 20 * 7;
+        // int divisao = 15 / 3;
+        // int modulo = 18 % 3;
+        // double resultado = (10 * 7 ) + (20 / 4);
+        // String concatenacao = "?";
+        // concatenacao = 1+1+1+"1";
 
-        System.out.println(concatenacao);
+        // System.out.println(concatenacao);
+        // concatenacao = 1+"1"+1+1;
 
-        concatenacao = 1+"1"+1+"1";
+        // System.out.println(concatenacao);
 
-        System.out.println(concatenacao);
+        // concatenacao = 1+"1"+1+"1";
 
-        concatenacao = "1"+1+1+1;
+        // System.out.println(concatenacao);
 
-        System.out.println(concatenacao);
+        // concatenacao = "1"+1+1+1;
 
-        concatenacao = "1"+(1+1+1);
+        // System.out.println(concatenacao);
 
-        System.out.println(concatenacao);
+        // concatenacao = "1"+(1+1+1);
 
-        int numero = 5;
+        // System.out.println(concatenacao);
 
-        numero = -numero;
-        System.out.println(numero);
+        // int numero = 5;
 
-        numero = numero * -1;
-        System.out.println(numero);
-        
+        // numero = -numero;
+        // System.out.println(numero);
+
+        // numero = numero * -1;
+        // System.out.println(numero);
+
+        // int numero = 5;
+
+        // // x repetição
+        // numero++;
+
+        // System.out.println(numero);
+
+        // boolean variavel = true;
+
+        // variavel = !variavel;
+        // System.out.println(variavel);
+
+        int numero1 = 6;
+        int numero2 = 6;
+        String resultado = numero1 == numero2 ? "verdadeiro" : "falso";
+        // if (numero1 == numero2)
+        // {
+        // resultado = "verdadeiro";
+        // }
+        // else
+        // {
+        // resultado = "falso";
+        // }
+        System.out.println(resultado);
     }
 }
