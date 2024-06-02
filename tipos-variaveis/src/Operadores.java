fe10359bd1b7b59c8b7b56fcffd38df246504f28
josiasdev <josiasmartins098@gmail.com>
2024-06-02 13:09:39 -0300

    update Operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -32,5 +32,13 @@ public class Operadores {
 
         System.out.println(concatenacao);
 
+        int numero = 5;
+
+        numero = -numero;
+        System.out.println(numero);
+
+        numero = numero * -1;
+        System.out.println(numero);
+        
     }
 }
